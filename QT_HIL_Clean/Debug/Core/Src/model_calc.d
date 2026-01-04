d76008fb90d17ba173a5556d3f5ba245d4e6d8c0
artemis13 <adilsalkimbaev29@gmail.com>
2026-01-05 00:07:19 +0500

    Fix .gitignore and remove build artifacts

--- QT_HIL_Clean/Debug/Core/Src/model_calc.d
+++
@@ -1,2 +0,0 @@
-Core/Src/model_calc.o: ../Core/Src/model_calc.c ../Core/Inc/config.h
-../Core/Inc/config.h:
--- QT_HIL_Clean/Debug/objects.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (12.3.rel1)
-################################################################################
-
-USER_OBJS := C:/Users/adils/STM32CubeIDE/workspace_1.17.0/QT_HIL_Clean/Core/Lib/libarm_cortexM7lfdp_math.a
-
-LIBS :=
-
